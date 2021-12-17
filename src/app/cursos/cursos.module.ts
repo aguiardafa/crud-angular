18880ca89514b75c0e0ee0fdee1bb3151a28bc4a
aguiardafa <diego.fernandes.aguiar@gmail.com>
2021-12-17 00:41:01 -0300

    feat(angularmaterial): customiza table do Angular Material e adiciona card e toolbar do Angular Material no Módulo de Cursos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [CursosComponent],
-  imports: [CommonModule, CursosRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    CursosRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
 })
 export class CursosModule {}
