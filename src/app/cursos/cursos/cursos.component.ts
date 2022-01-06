f80e0c3056ceff98abb0a2f6fa1105777f68abce
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-06 14:18:53 -0300

    feat(erro): implementa Dialog PopUp de Erro
    Adiciona MatDialog do Angular Material no AppMaterial, Cria Módulo Shared para que a Dialog possa ser utilizada em todo projeto e Cria Componente específico para Dialog de Erro.

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Curso } from '../models/curso';
 import { CursosService } from './../services/cursos.service';
@@ -13,13 +15,20 @@ export class CursosComponent implements OnInit {
   cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursoService: CursosService) {
+  constructor(private cursoService: CursosService, public dialog: MatDialog) {
     this.cursos$ = this.cursoService.findAll().pipe(
       catchError((error) => {
         console.log(error);
+        this.openDialog('Erro ao carregar os cursos!');
         return of([]);
       })
     );
+  }
+
+  openDialog(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
   ngOnInit(): void {}
